9acd2bf46263d5eea2c0d579b9133d98de622c02
Marty Games <martin.poliyanskiy+subsurf@gmail.com>
2025-11-29 16:49:04 -0800

    Update poki-sdk.js

--- html5/subway-surfers-new-york-thanksgiving-2025/poki-sdk.js
+++ html5/subway-surfers-new-york-thanksgiving-2025/poki-sdk.js
@@ -1,127 +1,179 @@
-! function(e) {
-    var n = {};
+document.xURL= "https://poki.com/";
 
-    function t(o) {
-        if (n[o]) return n[o].exports;
-        var r = n[o] = {
-            i: o,
-            l: !1,
-            exports: {}
-        };
-        return e[o].call(r.exports, r, r.exports, t), r.l = !0, r.exports
+if (typeof consoleLog== 'undefined') {
+  consoleLog= console.log;
+}
+
+var originalEval= eval;
+eval= function() {  
+  // consoleLog("--fx--eval--", arguments[0]);  
+  // debugger;  
+  arguments[0]= arguments[0].replace("aHR0cHM6Ly9wb2tpLmNvbS9zaXRlbG9jaw==", "I3ViZzIzNQ==");
+  arguments[0]= arguments[0].replace("'location'", "'xlocation'");
+  arguments[0]= arguments[0].replace("] = _0x3296f7;", "]==_0x3296f7;");
+  arguments[0]= arguments[0].replace("] = window[_0xcdc9(", "]==window[_0xcdc9(");
+  
+  
+  return originalEval.apply(this, arguments);
+}
+
+navigator.sendBeacon= function() {
+  consoleLog("--fx--navigator.sendBeacon--", arguments);
+}
+
+WebSocket= function() {
+  
+}
+
+xlocation= new Proxy(location, {
+  get: function(target, property, receiver) {
+    consoleLog("--fx--xlocation--get--property--", property);
+    let targetObj = target[property];
+    if (typeof targetObj == "function") {
+      return (...args) => target[property].apply(target, args);
+    } else {
+      if (property== "host" || property=="hostname") {
+        return "localhost";
+      }
+      if (property== "href") {
+        return "https://localhost/";
+      }
+      if (property== "origin") {
+        return "https://localhost/";
+      }
+      return targetObj;
     }
-    t.m = e, t.c = n, t.d = function(e, n, o) {
-        t.o(e, n) || Object.defineProperty(e, n, {
-            enumerable: !0,
-            get: o
-        })
-    }, t.r = function(e) {
-        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
-            value: "Module"
-        }), Object.defineProperty(e, "__esModule", {
-            value: !0
-        })
-    }, t.t = function(e, n) {
-        if (1 & n && (e = t(e)), 8 & n) return e;
-        if (4 & n && "object" == typeof e && e && e.__esModule) return e;
-        var o = Object.create(null);
-        if (t.r(o), Object.defineProperty(o, "default", {
-                enumerable: !0,
-                value: e
-            }), 2 & n && "string" != typeof e)
-            for (var r in e) t.d(o, r, function(n) {
-                return e[n]
-            }.bind(null, r));
-        return o
-    }, t.n = function(e) {
-        var n = e && e.__esModule ? function() {
-            return e.default
-        } : function() {
-            return e
-        };
-        return t.d(n, "a", n), n
-    }, t.o = function(e, n) {
-        return Object.prototype.hasOwnProperty.call(e, n)
-    }, t.p = "", t(t.s = 68)
-}({
-    68: function(e, n, t) {
-        var o = new(function() {
-            function e() {
-                var e = this;
-                this.queue = [], this.init = function(n) {
-                    return void 0 === n && (n = {}), new Promise((function(t, o) {
-                        e.enqueue("init", n, t, o)
-                    }))
-                }, this.rewardedBreak = function() {
-                    return new Promise((function(e) {
-                        e(!1)
-                    }))
-                }, this.noArguments = function(n) {
-                    return function() {
-                        e.enqueue(n)
-                    }
-                }, this.oneArgument = function(n) {
-                    return function(t) {
-                        e.enqueue(n, t)
-                    }
-                }, this.handleAutoResolvePromise = function() {
-                    return new Promise((function(e) {
-                        e()
-                    }))
-                }, this.handleAutoResolvePromiseObj = function() {
-                    return new Promise((function(e) {
-                        e()
-                    }))
-                }, this.throwNotLoaded = function() {
-                    console.debug("PokiSDK is not loaded yet. Not all methods are available.")
-                }
-            }
-            return e.prototype.enqueue = function(e, n, t, o) {
-                var r = {
-                    fn: e,
-                    options: n,
-                    resolveFn: t,
-                    rejectFn: o
-                };
-                this.queue.push(r)
-            }, e.prototype.dequeue = function() {
-                for (var e = function() {
-                        var e = n.queue.shift(),
-                            t = e,
-                            o = t.fn,
-                            r = t.options;
-                        "function" == typeof window.PokiSDK[o] ? (null == e ? void 0 : e.resolveFn) || (null == e ? void 0 : e.rejectFn) ? window.PokiSDK[o](r).then((function() {
-                            for (var n = [], t = 0; t < arguments.length; t++) n[t] = arguments[t];
-                            "function" == typeof e.resolveFn && e.resolveFn.apply(e, n)
-                        })).catch((function() {
-                            for (var n = [], t = 0; t < arguments.length; t++) n[t] = arguments[t];
-                            "function" == typeof e.rejectFn && e.rejectFn.apply(e, n)
-                        })) : void 0 !== (null == e ? void 0 : e.fn) && window.PokiSDK[o](r) : console.error("Cannot execute " + e.fn)
-                    }, n = this; this.queue.length > 0;) e()
-            }, e
-        }());
-        window.PokiSDK = {
-            init: o.init,
-            initWithVideoHB: o.init,
-            customEvent: o.throwNotLoaded,
-            commercialBreak: o.handleAutoResolvePromise,
-            rewardedBreak: o.rewardedBreak,
-            displayAd: o.throwNotLoaded,
-            destroyAd: o.throwNotLoaded,
-            getLeaderboard: o.handleAutoResolvePromiseObj
-        }, ["disableProgrammatic", "gameLoadingStart", "gameLoadingFinished", "gameInteractive", "roundStart", "roundEnd", "muteAd"].forEach((function(e) {
-            window.PokiSDK[e] = o.noArguments(e)
-        })), ["setDebug", "gameplayStart", "gameplayStop", "gameLoadingProgress", "happyTime", "setPlayerAge", "togglePlayerAdvertisingConsent", "toggleNonPersonalized", "setConsentString", "logError", "sendHighscore", "setDebugTouchOverlayController"].forEach((function(e) {
-            window.PokiSDK[e] = o.oneArgument(e)
-        }));
-        var r = function() {
-                var e, n = window.pokiSDKVersion;
-                n || (n = (e = RegExp("[?&]" + "ab" + "=([^&]*)").exec(window.location.search)) && decodeURIComponent(e[1].replace(/\+/g, " ")) || "v2");
-                return "./poki-sdk-core.js"
-            }(),
-            i = document.createElement("script");
-        i.setAttribute("src", r), i.setAttribute("type", "text/javascript"), i.onload = function() {
-            return o.dequeue()
-        }, document.head.appendChild(i)
-    }
+  },
+  set: function(target, property, receiver) {
+    consoleLog("--fx--xlocation--set--property--", property, receiver);
+    return true;
+  }
 });
+
+xwindow = new Proxy(window, {
+  get: function(target, property, receiver) {
+    // consoleLog("--fx--xWindow--property--", property, receiver);    
+    if (typeof target[property] == "function") {
+      return (...args) => target[property].apply(target,args);
+    } else {
+      if (property== "location") {
+        return target["xlocation"];        
+      }
+      // consoleLog("--fx--xwindow--targetObj--", targetObj);
+      return target[property];
+    }
+  }
+});
+// consoleLog(xwindow.location.href);
+// consoleLog("window.xlocation.href", window.xlocation.href);
+
+PokiSDK= function() {
+  // ***** UTILS *****
+  function loadJS(FILE_URL, callback) {
+    let scriptEle = document.createElement("script");
+  
+    scriptEle.setAttribute("src", FILE_URL);
+    scriptEle.setAttribute("type", "text/javascript");
+    scriptEle.setAttribute("async", true);
+  
+    document.body.appendChild(scriptEle);
+    
+    // Success
+    scriptEle.addEventListener("load", () => {
+      consoleLog("--fx--PokiSDK--loadJS Done--");
+      callback(true);
+    });
+    
+     // Error
+    scriptEle.addEventListener("error", () => {
+      consoleLog("--fx--PokiSDK--loadJS Error--");
+      callback(false);
+    });
+  }
+
+  this.getURLParam= function(name) {
+    return "";
+  }
+  
+  // ***** INIT *****
+  this.init= function() {
+    return new Promise((resolve, reject)=> {
+      resolve("InitDone");
+    });
+  }
+  
+  this.setDebug= function(debug) {
+    consoleLog("--fx--PokiSDK--setDebug--", debug);
+  }
+
+  this.setDebugTouchOverlayController= function (debug) {
+    consoleLog("--fx--PokiSDK--setDebugTouchOverlayController--", debug);
+  }
+  
+  this.isAdBlocked= function() {
+    consoleLog("--fx--PokiSDK--isAdBlocked--");    
+    return false;
+  }
+
+  this.happyTime= function(scale) {
+    consoleLog("--fx--PokiSDK--happyTime--", scale);    
+  }
+
+  // ***** LOADING *****  
+  this.gameLoadingStart= function(){
+    consoleLog("--fx--PokiSDK--gameLoadingStart--");
+  }
+  
+  this.gameLoadingProgress= function(progress){
+    consoleLog("--fx--PokiSDK--gameLoadingProgress--", progress);
+  }
+  
+  this.gameLoadingFinished= function(){
+    consoleLog("--fx--PokiSDK--gameLoadingFinished--");
+  }
+
+  // ***** GAME CONTROL *****
+  this.gameplayStart= function(){
+    consoleLog("--fx--PokiSDK--gameplayStart--");
+  }
+
+  this.gameplayStop= function() {
+    consoleLog("--fx--PokiSDK--gameplayStop--");
+  }
+
+  // ***** ADS CONTROL *****
+  this.commercialBreak= function(){
+    consoleLog("--fx--PokiSDK--commercialBreak--");
+    return new Promise((resolve, reject)=> {
+      loadJS("https://www.ubg235.com/ads/commercial.js", resolve);  
+    });
+  }
+
+  this.rewardedBreak= function() {
+   consoleLog("--fx--PokiSDK--rewardedBreak--");
+    return new Promise((resolve, reject)=> {
+      loadJS("https://www.ubg235.com/ads/rewarded.js", resolve);
+    }); 
+  }
+
+  this.displayAd= function() {
+    consoleLog("--fx--PokiSDK--displayAd--", arguments);
+  }
+
+  this.destroyAd= function() {
+    consoleLog("--fx--PokiSDK--destroyAd--", arguments);
+  }
+}
+
+PokiSDK.prototype.initWithVideoHB= function() {
+  consoleLog("--fx--PokiSDK--initWithVideoHB--");
+  return new Promise((resolve, reject)=> {
+    resolve("")
+  });
+}
+
+PokiSDK.prototype.customEvent= function() {
+  consoleLog("--fx--PokiSDK--customEvent--");
+}
+
+PokiSDK= new PokiSDK();
